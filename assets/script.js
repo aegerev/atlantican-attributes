8768794e163bb19c13689398f632c0daae1fd398
alex <alexandersegerev@gmail.com>
2024-11-20 13:38:51 -0500

    Added JS

---
+++ assets/script.js
@@ -0,0 +1,60 @@
+let site1El = document.querySelector(".site1");
+let site2El = document.querySelector(".site2");
+let site3El = document.querySelector(".site3");
+let site4El = document.querySelector(".site4");
+let site5El = document.querySelector(".site5");
+let site6El = document.querySelector(".site6");
+let site7El = document.querySelector(".site7");
+let site8El = document.querySelector(".site8");
+let site9El = document.querySelector(".site9");
+let site10El = document.querySelector(".site10");
+
+site1El.children[0].textContent = "Mad Money's Official Website";
+site1El.children[1].setAttribute("href", "https://www.cnbc.com/mad-money/");
+site1El.children[1].children[0].setAttribute("style", "padding:10px;");
+
+site2El.children[0].textContent = "Mad Money Back To School Tour";
+site2El.children[1].setAttribute("href", "https://www.youtube.com/watch?v=MmnQPjRMSCc&ab_channel=CNBCTelevision");
+site2El.children[1].children[0].setAttribute("style", "padding:10px;");
+
+site3El.children[0].textContent = "The Lightning Round";
+site3El.children[1].setAttribute("href", "https://x.com/MadMoneyOnCNBC/status/850030358519099393");
+site3El.children[1].children[0].setAttribute("style", "padding:10px;");
+
+site4El.children[0].textContent = "The Fed Rant Heard 'Round the World";
+site4El.children[1].setAttribute("href", "https://www.youtube.com/watch?v=TaKnDMv6ceg&ab_channel=JimCramerPicks");
+site4El.children[1].children[0].setAttribute("style", "padding:10px;");
+
+site2El.children[0].textContent = "Site 2";
+site2El.children[1].setAttribute("href", "https://twitter.com");
+site2El.children[1].children[0].setAttribute("style", "padding:10px;");
+
+// site2El.children[0].textContent = "Site 2";
+// site2El.children[1].setAttribute("href", "https://twitter.com");
+// site2El.children[1].children[0].setAttribute("src", "assets/images/image_2.jpg");
+// site2El.children[1].children[0].setAttribute("alt", "group brainstorm");
+// site2El.children[1].children[0].setAttribute("style", "padding:10px;");
+
+// site2El.children[0].textContent = "Site 2";
+// site2El.children[1].setAttribute("href", "https://twitter.com");
+// site2El.children[1].children[0].setAttribute("src", "assets/images/image_2.jpg");
+// site2El.children[1].children[0].setAttribute("alt", "group brainstorm");
+// site2El.children[1].children[0].setAttribute("style", "padding:10px;");
+
+// site2El.children[0].textContent = "Site 2";
+// site2El.children[1].setAttribute("href", "https://twitter.com");
+// site2El.children[1].children[0].setAttribute("src", "assets/images/image_2.jpg");
+// site2El.children[1].children[0].setAttribute("alt", "group brainstorm");
+// site2El.children[1].children[0].setAttribute("style", "padding:10px;");
+
+// site2El.children[0].textContent = "Site 2";
+// site2El.children[1].setAttribute("href", "https://twitter.com");
+// site2El.children[1].children[0].setAttribute("src", "assets/images/image_2.jpg");
+// site2El.children[1].children[0].setAttribute("alt", "group brainstorm");
+// site2El.children[1].children[0].setAttribute("style", "padding:10px;");
+
+// site2El.children[0].textContent = "Site 2";
+// site2El.children[1].setAttribute("href", "https://twitter.com");
+// site2El.children[1].children[0].setAttribute("src", "assets/images/image_2.jpg");
+// site2El.children[1].children[0].setAttribute("alt", "group brainstorm");
+// site2El.children[1].children[0].setAttribute("style", "padding:10px;");
