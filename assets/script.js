3a242166197965c7da1e0287f14011073ffeb78e
alex <alexandersegerev@gmail.com>
2024-11-22 12:34:19 -0500

    Added new items

--- assets/script.js
+++ assets/script.js
@@ -13,27 +13,29 @@ site1El.children[0].textContent = "Mad Money's Official Website";
 site1El.children[1].setAttribute("href", "https://www.cnbc.com/mad-money/");
 site1El.children[1].children[0].setAttribute("style", "padding:10px;");
 
-site2El.children[0].textContent = "Mad Money Back To School Tour";
+site2El.children[0].textContent = "MAD Money Back-To-School Tour 2022";
 site2El.children[1].setAttribute("href", "https://www.youtube.com/watch?v=MmnQPjRMSCc&ab_channel=CNBCTelevision");
 site2El.children[1].children[0].setAttribute("style", "padding:10px;");
 
-site3El.children[0].textContent = "The Lightning Round";
-site3El.children[1].setAttribute("href", "https://x.com/MadMoneyOnCNBC/status/850030358519099393");
+site3El.children[0].textContent = "The MAD Eagle";
+site3El.children[1].setAttribute("href", "https://www.youtube.com/watch?v=v_CzsaUNURc&ab_channel=NFLFilms");
 site3El.children[1].children[0].setAttribute("style", "padding:10px;");
 
-site4El.children[0].textContent = "The Fed Rant Heard 'Round the World";
-site4El.children[1].setAttribute("href", "https://www.youtube.com/watch?v=TaKnDMv6ceg&ab_channel=JimCramerPicks");
+site4El.children[0].textContent = "The Lightning Round";
+site4El.children[1].setAttribute("href", "https://x.com/MadMoneyOnCNBC/status/850030358519099393");
 site4El.children[1].children[0].setAttribute("style", "padding:10px;");
 
-site2El.children[0].textContent = "Site 2";
-site2El.children[1].setAttribute("href", "https://twitter.com");
-site2El.children[1].children[0].setAttribute("style", "padding:10px;");
+site5El.children[0].textContent = "The Fed Rant Heard 'Round the World";
+site5El.children[1].setAttribute("href", "https://www.youtube.com/watch?v=TaKnDMv6ceg&ab_channel=JimCramerPicks");
+site5El.children[1].children[0].setAttribute("style", "padding:10px;");
 
-// site2El.children[0].textContent = "Site 2";
-// site2El.children[1].setAttribute("href", "https://twitter.com");
-// site2El.children[1].children[0].setAttribute("src", "assets/images/image_2.jpg");
-// site2El.children[1].children[0].setAttribute("alt", "group brainstorm");
-// site2El.children[1].children[0].setAttribute("style", "padding:10px;");
+site6El.children[0].textContent = "Cramer On the Sea";
+site6El.children[1].setAttribute("href", "https://www.youtube.com/watch?v=xrs6oK3Mxa8&list=PLVbP054jv0KpV2leJ9HHIMqZEkCPX-iPV&index=92&ab_channel=CNBCTelevision");
+site6El.children[1].children[0].setAttribute("style", "padding:10px;");
+
+site7El.children[0].textContent = "Iron Man + Mad Money = ??";
+site7El.children[1].setAttribute("href", "https://www.youtube.com/watch?v=EMWANB0AAns&ab_channel=KnoxGibson");
+site7El.children[1].children[0].setAttribute("style", "padding:10px;");
 
 // site2El.children[0].textContent = "Site 2";
 // site2El.children[1].setAttribute("href", "https://twitter.com");
